d19ae7bd28fa35ab97e3a268aa788cadc1c5c357
Navan Chauhan <navan.chauhan@strongdm.com>
2025-08-27 19:33:04 -0700

    Swift 6: make PrimitiveView.body nonisolated(unsafe) to satisfy View protocol under @MainActor GenericView

--- Sources/SwiftTUI/ViewGraph/PrimitiveView.swift
+++ Sources/SwiftTUI/ViewGraph/PrimitiveView.swift
@@ -18,7 +18,7 @@ import Foundation
 protocol PrimitiveView: GenericView {}
 
 extension PrimitiveView {
-    public var body: Never { fatalError("Cannot evaluate body of primitive view") }
+    nonisolated(unsafe) public var body: Never { fatalError("Cannot evaluate body of primitive view") }
 }
 
 extension View {
