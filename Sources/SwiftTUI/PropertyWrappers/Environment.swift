5202b2d970e46f7b14efa68eef8715d020f8a66b
Navan Chauhan <navan.chauhan@strongdm.com>
2025-08-28 22:46:04 -0700

    Environment: avoid assertion crash when @Environment accessed before instantiation; return defaults. Add early-access unit test.

--- Sources/SwiftTUI/PropertyWrappers/Environment.swift
+++ Sources/SwiftTUI/PropertyWrappers/Environment.swift
@@ -1,58 +1,56 @@
 import Foundation
 
 public protocol EnvironmentKey {
-    associatedtype Value
-    static var defaultValue: Value { get }
+   associatedtype Value
+   static var defaultValue: Value { get }
 }
 
 public struct EnvironmentValues {
-    var values: [ObjectIdentifier: Any] = [:]
-    public subscript<K: EnvironmentKey>(key: K.Type) -> K.Value {
-        get { values[ObjectIdentifier(key)] as? K.Value ?? K.defaultValue }
-        set { values[ObjectIdentifier(key)] = newValue }
-    }
+   var values: [ObjectIdentifier: Any] = [:]
+   public subscript<K: EnvironmentKey>(key: K.Type) -> K.Value {
+       get { values[ObjectIdentifier(key)] as? K.Value ?? K.defaultValue }
+       set { values[ObjectIdentifier(key)] = newValue }
+   }
 }
 
 @propertyWrapper
 public struct Environment<T>: AnyEnvironment {
-    let keyPath: KeyPath<EnvironmentValues, T>
+   let keyPath: KeyPath<EnvironmentValues, T>
 
-    public init(_ keyPath: KeyPath<EnvironmentValues, T>) {
-        self.keyPath = keyPath
-    }
+   public init(_ keyPath: KeyPath<EnvironmentValues, T>) {
+       self.keyPath = keyPath
+   }
 
-    var valueReference = EnvironmentReference()
+   var valueReference = EnvironmentReference()
 
-    public var wrappedValue: T {
-        get {
-            guard let node = valueReference.node else {
-                assertionFailure("Attempting to access @Environment variable before view is instantiated")
-                return EnvironmentValues()[keyPath: keyPath]
-            }
-            let environmentValues = makeEnvironment(node: node, transform: { _ in })
-            return environmentValues[keyPath: self.keyPath]
-        }
-        set {}
-    }
+   public var wrappedValue: T {
+       get {
+           // Gracefully return default when accessed before instantiation or after teardown
+           guard let node = valueReference.node else { return EnvironmentValues()[keyPath: keyPath] }
+           let environmentValues = makeEnvironment(node: node, transform: { _ in })
+           return environmentValues[keyPath: self.keyPath]
+       }
+       set {}
+   }
 
-    private func makeEnvironment(node: Node, transform: (inout EnvironmentValues) -> Void) -> EnvironmentValues {
-        if let parent = node.parent {
-            return makeEnvironment(node: parent) {
-                node.environment?(&$0)
-                transform(&$0)
-            }
-        }
-        var environmentValues = EnvironmentValues()
-        node.environment?(&environmentValues)
-        transform(&environmentValues)
-        return environmentValues
-    }
+   private func makeEnvironment(node: Node, transform: (inout EnvironmentValues) -> Void) -> EnvironmentValues {
+       if let parent = node.parent {
+           return makeEnvironment(node: parent) {
+               node.environment?(&$0)
+               transform(&$0)
+           }
+       }
+       var environmentValues = EnvironmentValues()
+       node.environment?(&environmentValues)
+       transform(&environmentValues)
+       return environmentValues
+   }
 }
 
 protocol AnyEnvironment {
-    var valueReference: EnvironmentReference { get }
+   var valueReference: EnvironmentReference { get }
 }
 
 class EnvironmentReference {
-    weak var node: Node?
+   weak var node: Node?
 }
